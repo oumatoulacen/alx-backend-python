55a536d44bbc42faa6c329b1973483ae0878a235
oumatoulacen <oumatoulacen@gmail.com>
2024-01-11 22:07:52 +0100

    basic annotations: variables

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 ''' complex types annotation'''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''sum elements of a list'''
     return sum(input_list)
