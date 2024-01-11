f60cf886dce5b6982430ec7a34105f0ca319cd2c
oumatoulacen <oumatoulacen@gmail.com>
2024-01-12 00:36:03 +0100

    Complex types - functions/Callable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+'''functions'''
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''multiplier'''
+    def fun (multiplier: float) -> float:
+        return multiplier * multiplier
+    return fun
