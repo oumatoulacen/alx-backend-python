4d93d33616375578456d97613ea2d482502e78e2
oumatoulacen <oumatoulacen@gmail.com>
2024-01-12 03:12:09 +0100

    look into TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 ''' Let's duck type an iterable object '''
-from typing import List, Iterable, Sequence, Tuple
+from typing import Union, Mapping, Any, TypeVar
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''length with elements in a tuple'''
-    return [(i, len(i)) for i in lst]
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None):
+    ''' Get the value of the given key'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
