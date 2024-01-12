e44e067ce276fc7f2803a039d9eca69289411cde
oumatoulacen <oumatoulacen@gmail.com>
2024-01-12 03:40:01 +0100

    look into TypeVar

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 ''' Let's duck type an iterable object '''
-from typing import List, Iterable, Sequence, Tuple
+from typing import List, Union, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''length with elements in a tuple'''
-    return [(i, len(i)) for i in lst]
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
