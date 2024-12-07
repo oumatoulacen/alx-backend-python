41d8bb6a31c03607e82b582a25a45bdd3585077a
oumatoulacen <username@users.noreply.github.com>
2024-12-07 21:07:07 +0100

    Add a decorator to log SQL queries and implement user fetching

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Log queries """
+import time
+import sqlite3
+import functools
+
+#### decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f'{func.__name__} - {args[0]!r} - {end_time - start_time}')
+        return result
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users("SELECT * FROM users")
