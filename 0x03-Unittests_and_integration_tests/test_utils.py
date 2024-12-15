e23b0f3362e96becda3605bedd3ecfef006c146a
oumatoulacen <username@users.noreply.github.com>
2024-12-15 04:00:24 +0100

    Refactor test_access_nested_map_exception to use parameterized inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,8 +20,12 @@ class TestAccessNestedMap(TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    def test_access_nested_map_exception(self):
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
         """Test access_nested_map with invalid path.
         """
         with self.assertRaises(KeyError):
-            access_nested_map({"a": 1}, ("a", "b"))
+            access_nested_map(nested_map, path)
