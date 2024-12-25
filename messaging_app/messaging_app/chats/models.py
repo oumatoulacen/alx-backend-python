ddae0377d98c3831517c5623bc3682ee0509e0a1
oumatoulacen <username@users.noreply.github.com>
2024-12-25 23:36:54 +0100

    Add models for User, Conversation, and Message in messaging_app

--- messaging_app/messaging_app/chats/models.py
+++ messaging_app/messaging_app/chats/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+# Database Specification
+# Entities and Attributes
+
+# User:
+# user_id (Primary Key, UUID, Indexed)
+# first_name (VARCHAR, NOT NULL)
+# last_name (VARCHAR, NOT NULL)
+# email (VARCHAR, UNIQUE, NOT NULL)
+# password_hash (VARCHAR, NOT NULL)
+# phone_number (VARCHAR, NULL)
+# role (ENUM: 'guest', 'host', 'admin', NOT NULL)
+# created_at (TIMESTAMP, DEFAULT CURRENT_TIMESTAMP)
+
+# Message:
+# message_id (Primary Key, UUID, Indexed)
+# sender_id (Foreign Key, references User(user_id))
+# message_body (TEXT, NOT NULL)
+# sent_at (TIMESTAMP, DEFAULT CURRENT_TIMESTAMP)
+
+# Conversation:
+# conversation_id (Primary Key, UUID, Indexed)
+# participants_id (Foreign Key, references User(user_id)
+# created_at (TIMESTAMP, DEFAULT CURRENT_TIMESTAMP)
+
+# Using the tables definition described above,
+
+# Create the user Model an extension of the Abstract user for values not defined in the built-in Django User model
+# Create the conversation model that tracks which users are involved in a conversation
+# Create the message model containing the sender, conversation as defined in the shared schema attached to this project
+
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True)
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE)
