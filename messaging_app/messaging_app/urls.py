eae135656170a69c46d8db95a1ec29089703c9f4
oumatoulacen <username@users.noreply.github.com>
2024-12-26 01:13:04 +0100

    Add URL routing for Conversation and Message viewsets

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from chats.urls import router as chats_router
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
-
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(chats_router.urls)),
 ]
