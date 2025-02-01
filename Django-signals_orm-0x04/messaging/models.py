62f6cc7ebab3c999cbcd597a0bdfd8f10289918d
oumatoulacen <username@users.noreply.github.com>
2025-02-01 01:28:58 +0100

    Refactor Message and Notification models to use timestamp and is_read fields

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,30 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
-    sent_at = models.DateTimeField(auto_now_add=True)
-    read_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        ordering = ('sent_at',)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     def mark_as_read(self):
-        self.read_at = timezone.now()
+        self.is_read = True
         self.save()
 
 class Notification(models.Model):
     receiver = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    sent_at = models.DateTimeField(auto_now_add=True)
-    read_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        ordering = ('sent_at',)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     def mark_as_read(self):
-        self.read_at = timezone.now()
+        self.is_read = True
         self.save()
